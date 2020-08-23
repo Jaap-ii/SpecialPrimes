25ae7a51ed1190cfad02d1c16fd1bd410f03ee04
Jaap Okkinga <jaap.okkinga@prutsenzo.nl>
2020-08-23 12:55:33 +0200

    oddbitarray now serializable

--- OddBitArray.cs
+++ OddBitArray.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace SpecialPrimes
 {
-    public class OddBitArray
+    [Serializable]
+    public class OddBitArray : ISerializable
     {
-        private readonly BitArray bitArray;
-        public OddBitArray(int highestIndex)
+        private readonly BitArray _bitArray;
+        private readonly int _startIndex;
+        public OddBitArray(int startIndex, int highestIndex)
         {
-            bitArray = new BitArray(OddIndex(highestIndex));
+            if (startIndex % 2 == 0)
+                throw new Exception("OddBitArray is meant to store odd numbers, idiot!");
+            _startIndex = startIndex;
+            _bitArray = new BitArray(OddIndex(highestIndex));
         }
 
-        public OddBitArray(BitArray bitArray)
+        protected OddBitArray(SerializationInfo info, StreamingContext context)
         {
-            this.bitArray = bitArray;
+            _startIndex = (int)info.GetValue("start", typeof(int));
+            _bitArray = (BitArray)info.GetValue("data", typeof(BitArray));
         }
 
         private int OddIndex(int index)
         {
-            return (index - 1)/2;
+            return (index - _startIndex) /2;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("start", _startIndex);
+            info.AddValue("data", _bitArray);
         }
 
         public bool this[int index]
         {
-            get => bitArray[OddIndex(index)];
-            set => bitArray[OddIndex(index)] = value;
-        }
-        public BitArray rawBitArray
-        {
-            get => bitArray;
+            get => _bitArray[OddIndex(index - _startIndex)];
+            set => _bitArray[OddIndex(index - _startIndex)] = value;
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -16,37 +16,36 @@ namespace SpecialPrimes
     {
         static void Main(string[] args)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var boe = assembly.GetManifestResourceNames();
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var boe = assembly.GetManifestResourceNames();
+            //var sw = new Stopwatch();
+            //sw.Start();
+            //using (Stream stream = assembly.GetManifestResourceStream("SpecialPrimes.DataFile2.dat"))
+            //sw.Stop();
+            var atkin = new SieveOfAtkin(0, int.MaxValue-1);
+            var boe5 = atkin.ToList();
+
+            var primeTool = new PrimeTool();
+            var oddBits = new OddBitArray(1, 1000);
             var sw = new Stopwatch();
             sw.Start();
-            using (Stream stream = assembly.GetManifestResourceStream("SpecialPrimes.DataFile2.dat"))
-            {
-                BinaryFormatter formatter2 = new BinaryFormatter();
-                BitArray boe3 = formatter2.Deserialize(stream) as BitArray;
-            }
-            sw.Stop();
-            var primeTool = new PrimeTool();
-            var oddBits = new OddBitArray(int.MaxValue);
-            for (int i = 3; i < int.MaxValue; i = i + 2)
+            for (int i = 1 ; i < 1000; i = i + 2)
             {
                 if (FermatLittleTheorem.IsPrime(i) && primeTool.IsPrime(i))
-                    oddBits[i] ^= true;
+                        oddBits[i] ^= true;
                 if ((i % 100000) < 2)
                     Console.Write(".");
                 if ((i % 10000000) < 2)
                 {
                     Console.WriteLine();
-                    Console.WriteLine(i);
-                    Console.WriteLine(int.MaxValue);
+                    Console.WriteLine($"{i} of {int.MaxValue}, took {sw.ElapsedMilliseconds} ms");
+                    sw.Restart();
                 }
             }
+            sw.Stop();
             BinaryFormatter formatter = new BinaryFormatter();
-            FileStream fs = new FileStream(@"c:\temp\DataFile02.dat", FileMode.Create);
-            formatter.Serialize(fs, oddBits.rawBitArray);
-            //var b = new BitArray(int.MaxValue / 8);
-            //FileStream fs2 = new FileStream(@"c:\temp\DataFile2.dat", FileMode.Create);
-            //formatter.Serialize(fs2, b);
+             FileStream fs2 = new FileStream(@"c:\temp\OddBits2.dat", FileMode.Create);
+            formatter.Serialize(fs2, oddBits);
 
         }
 
