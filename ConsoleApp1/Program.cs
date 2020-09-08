011179269c0c6de979a7528c99100d1e03d890bc
Jaap Okkinga <jaap.okkinga@di-vision.nl>
2020-09-08 21:50:57 +0200

    first version of superfastprimefinder

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,23 +10,85 @@ using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Reflection;
 using SpecialPrimesDVC;
+using System.IO.Compression;
+using SuperfastSpecialPrimes;
 
 namespace SpecialPrimes
 {
     class Program
     {
-        static void Main2(string[] args)
+        static void Main(string[] args)
         {
+            Primer.boe();
+            return;
             int upper = int.MaxValue;
-            int lower = upper - 25000000; // 1000000;
+            int lower = 1;
             var sw = new Stopwatch();
             sw.Start();
-            var boe = JaapHisSolver.Solve(lower, upper);
-            sw.Stop();
-            Console.WriteLine($"found {boe.Count} took {sw.ElapsedMilliseconds} ms");
-            sw.Reset();
+            //var sp = new SpecialPrimeFinder(PrimeFinderFactory.CreatePrimeFinder(lower, upper));
+            //int boe = 0;
+            //var oba = new OddBitArray(lower,upper);
+            //foreach (int i in sp.GetSpecialPrimes(lower, upper))
+            //{
+            //    oba[i] = true;
+            //    boe++;
+            //};
+            //sw.Stop();
+
+            //Console.WriteLine($"found {boe} took {sw.ElapsedMilliseconds} ms");
+            //sw.Reset();
+            //var formatter = new BinaryFormatter();
+            //var fs = new FileStream(@"c:\temp\SpecialPrimeOddBitArray.dat", FileMode.Create);
+            //formatter.Serialize(fs, oba);
+            // Assembly.GetExecutingAssembly()
+            // GetManifestoResourceStream()
+            OddBitArray boe;
+            var formatter = new BinaryFormatter();
+            using (Stream stream = new FileStream(@"c:\temp\SpecialPrimeOddBitArray.dat", FileMode.Open))
+            {
+                boe = formatter.Deserialize(stream) as OddBitArray;
+            };
+            var oba1 = new SuperfastSpecialPrimes.OddBitArray(1,1000001);
+            for (int i=1;i<=1000001;i=i+2)
+            {
+                oba1[i] = boe[i];
+            }
+            using (var fs = new FileStream(@"c:\temp\SpecialPrimeOddBitArraySmall.dat", FileMode.Create))
+            {
+                formatter.Serialize(fs, oba1);
+            }
+            return;
+            using (var ms = new MemoryStream())
+            {
+                formatter.Serialize(ms, boe);
+                ms.Position = 0;
+                using (FileStream compressedFileStream = File.Create(@"c:\temp\SpecialPrimeOddBitArray3.dat"))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                    {
+                        ms.CopyTo(compressionStream);
+                        compressionStream.Close();
+                    }
+                }
+            }
+
+            var outputStream = new MemoryStream();
+            using (var inputStream = File.Open(@"c:\temp\SpecialPrimeOddBitArray3.dat", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var gzip = new GZipStream(inputStream, CompressionMode.Decompress))
+            {
+                gzip.CopyTo(outputStream);
+
+                gzip.Close();
+                inputStream.Close();
+
+                // After writing to the MemoryStream, the position will be the size
+                // of the decompressed file, we should reset it back to zero before returning.
+                outputStream.Position = 0;
+            }
+            var boe2 = formatter.Deserialize(outputStream) as OddBitArray;
         }
-        static void Main(string[] args)
+        static void Main2(string[] args)
         { //SpecialPrime.FindSpecialPrimes(1,10).ForEach(i => Console.Write("{0} ,", i));
             var testSet = BuildTests();
             var sw = new Stopwatch();
---
+++ SuperfastSpecialPrimes/Primer.cs
@@ -0,0 +1,23 @@
+﻿using SevenZip.Compression.LZMA;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SuperfastSpecialPrimes
+{
+    public class Primer
+    {
+        private static OddBitArray obaSmall { get; set; }
+        public static void boe()
+        {
+            var ass = Assembly.GetExecutingAssembly();
+            var formatter = new BinaryFormatter();
+            using (var stream = ass.GetManifestResourceStream("SuperfastSpecialPrimes.SpecialPrimeOddBitArraySmall.dat"))
+            {
+                obaSmall = formatter.Deserialize(stream) as OddBitArray;
+            }
+        }
+    }
+}
